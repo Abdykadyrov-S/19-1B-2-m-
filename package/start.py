d02ed383d5166438bdb4843507afb453b78bf930
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-07-19 20:56:31 +0600

    first commit

---
+++ package/start.py
@@ -0,0 +1,6 @@
+from test import *
+
+sum(12,7)
+sub(45,7)
+
+
---
+++ package/test.py
@@ -0,0 +1,14 @@
+def sum(a, b):
+    print(a + b)
+
+def sub(a, b):
+    print(a - b)
+
+def mult(a, b):
+    print(a * b)
+
+if __name__ == "__main__":
+    sum(7, 2)
+    sub(7, 2)
+
+    print(__name__)
